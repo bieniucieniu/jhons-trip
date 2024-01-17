6168923972e35db0b6e07be0c761dfa952638803
bieniucieniu <mikibien@outlook.com>
2024-01-17 17:15:45 +0100

    add filter

--- journey/src/app/components/journeys/journeys.component.ts
+++ journey/src/app/components/journeys/journeys.component.ts
@@ -4,18 +4,40 @@ import { Journey } from "../../journey"
 import { CommonModule } from "@angular/common"
 import { JourneyComponent } from "../journey/journey.component"
 import { v4 as uuidv4 } from "uuid"
+import { FormBuilder, ReactiveFormsModule } from "@angular/forms"
+
+interface Filters {
+  country: string
+  start: string
+  end: string
+  maxPrice: number
+  slots: number
+}
 
 @Component({
   selector: "app-journeys",
   standalone: true,
-  imports: [CommonModule, JourneyComponent],
+  imports: [CommonModule, JourneyComponent, ReactiveFormsModule],
   templateUrl: "./journeys.component.html",
   styleUrl: "./journeys.component.scss",
 })
 export class JourneysComponent {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private formBuilder: FormBuilder,
+  ) {}
+
   journeys: Journey[] | undefined = undefined
+  filteredJourneys: Journey[] | undefined = undefined
   booking: string = ""
+
+  filtersControlls = this.formBuilder.group<Filters>({
+    country: "",
+    start: "",
+    end: "",
+    maxPrice: 0,
+    slots: 0,
+  })
 
   ngOnInit() {
     this.getJourneys().subscribe((data) => {
@@ -24,6 +46,7 @@ export class JourneysComponent {
         id: uuidv4(),
         booked: 0,
       }))
+      this.filteredJourneys = this.journeys
     })
   }
 
@@ -54,5 +77,29 @@ export class JourneysComponent {
 
   getJourneys() {
     return this.http.get<{ journeys: Journey[] }>(`../assets/data.json`)
+  }
+
+  setFilteredJourneys() {
+    const filters = this.filtersControlls.value
+    this.filteredJourneys =
+      this.journeys?.filter((e) => {
+        if (!!filters.end && new Date(filters.end).getTime() > e.end) {
+          return false
+        }
+        if (!!filters.start && new Date(filters.start).getTime() < e.end) {
+          return false
+        }
+        if (!!filters.slots && e.maxPeople - e.booked < filters.slots) {
+          return false
+        }
+        if (!!filters.country && e.country.startsWith(filters.country)) {
+          return false
+        }
+        if (!!filters.maxPrice && e.price > filters.maxPrice) {
+          return false
+        }
+
+        return true
+      }) ?? []
   }
 }
