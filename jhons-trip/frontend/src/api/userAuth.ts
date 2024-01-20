1b67e132ba9cbc0934938a1f8c30c09cc6bd9fcd
bieniucieniu <mikibien@outlook.com>
2024-01-20 13:38:11 +0100

    extend user auth

---
+++ jhons-trip/frontend/src/api/userAuth.ts
@@ -0,0 +1,57 @@
+import getBaseUrl from "@/lib/getBaseUrl";
+
+export async function genUser(
+  username: string,
+  password: string,
+  callback?: (res: { token?: string; error?: any }) => void,
+) {
+  try {
+    const res = await fetch(getBaseUrl() + "api/signin", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+    const body = await res.json();
+    if (callback !== undefined) callback(body);
+    return res;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+type User =
+  | {
+      username: string;
+      userID: number;
+      privilege: number;
+    }
+  | undefined;
+
+export async function getUser(
+  callback?: (res: { user?: User; error?: any }) => void,
+) {
+  try {
+    const res = await fetch(getBaseUrl() + "api/auth", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const body = (await res.json()) as {
+      user: User;
+      error: any;
+    };
+
+    if (callback !== undefined) callback(body);
+    return res;
+  } catch (e) {
+    console.log(e);
+  }
+}
